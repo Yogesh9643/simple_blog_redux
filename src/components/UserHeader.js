6cc4f7fa3e2e214563a35feb1f6adff2869028ed
Yogesh9643 <rajputyogesh563@gmail.com>
2020-10-21 13:40:56 +0530

    attached reducer to userheader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,6 +3,10 @@ import {connect} from 'react-redux';
 import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component{
+  componentDidMount(){
+      this.props.fetchUser(this.props.userId);
+  }
+
    render(){
        return <div>UserHeader</div>;
    }
